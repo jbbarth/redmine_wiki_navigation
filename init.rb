496c2c5a8e4f15179d7437731b5cc3b73f2a4baa
jbbarth <jeanbaptiste.barth@gmail.com>
2009-11-24 22:12:52 +0100

    Initial commit ; empty plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_wiki_navigation do
+  name 'Redmine Wiki Navigation plugin'
+  author 'Jean-Baptiste BARTH <jeanbaptiste.barth@gmail.com>'
+  description 'This plugin displays a navigation tree in wiki sidebar'
+  version '0.1'
+end
